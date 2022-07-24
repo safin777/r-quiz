b4843ab11bd2be8f896463211f3035d986042e1a
safin777 <sayedislamsafin7770555@gmail.com>
2022-07-24 23:10:04 +0600

    Login page completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import '../styles/App.css'
 import Layout from './Layout'
+import Login from './pages/Login'
 //  import Home from './pages/Home'
-import Signup from './pages/Signup'
+//import Signup from './pages/Signup'
 
 function App() {
   return (
     <Layout>
       {/* <Home /> */}
-      <Signup />
+      {/* <Signup /> */}
+      <Login/>
     </Layout>
   )
 }
