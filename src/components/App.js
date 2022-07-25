76e8f1d58858eb0a229ca92158813ffa7d61c1d5
safin777 <sayedislamsafin7770555@gmail.com>
2022-07-25 22:24:38 +0600

    lesson-9 Auth Context created

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,22 @@ import Quiz from './pages/Quiz'
 import Result from './pages/Result'
 import Signup from './pages/Signup'
 
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from '../context/AuthContext'
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route exact path='/' element={<Home/>} />
-          <Route exact path='/login' element={<Login/>} /> 
-          <Route exact path='/signup' element={<Signup/>} />
-          <Route exact path='/quiz' element={<Quiz/>} />
-          <Route exact path='/result' element={<Result/>} />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/quiz" element={<Quiz />} />
+            <Route exact path="/result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </Router>
   )
 }
